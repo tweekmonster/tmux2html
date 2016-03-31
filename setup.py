e0041454a144790c08b9f9e945448a0e881ac09c
Tommy Allen <tommy@esdf.io>
2016-03-31 00:48:29 -0400

    Bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ then parses their contents into HTML.
 
 setup(
     name='tmux2html',
-    version='0.1.2',
+    version='0.1.3',
     author='Tommy Allen',
     author_email='tommy@esdf.io',
     description=desc,
